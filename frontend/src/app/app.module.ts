cb95837a47b901b026b29e4eea5e00ed01f87564
Noelle Everage <neverage84@yahoo.com>
2020-08-12 15:27:15 -0700

    added navbar component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ContactInfoComponent } from './contact-info/contact-info.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DisplayComponent } from './display/display.component';
 import { Routes, RouterModule } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
 const routes: Routes = [
   {path: 'dashboard', component: DashboardComponent},
@@ -31,7 +32,8 @@ const routes: Routes = [
     SystemTimeComponent,
     ContactInfoComponent,
     DashboardComponent,
-    DisplayComponent
+    DisplayComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
