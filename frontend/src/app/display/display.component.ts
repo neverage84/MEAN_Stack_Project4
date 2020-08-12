d810e5c224eec8673c1daa3a9b4a15fb5ed5cd39
Noelle Everage <neverage84@yahoo.com>
2020-08-12 16:33:08 -0700

    added backend folder with package.json and babel

--- frontend/src/app/display/display.component.ts
+++ frontend/src/app/display/display.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-display',
+  selector: 'display',
   templateUrl: './display.component.html',
   styleUrls: ['./display.component.css']
 })
