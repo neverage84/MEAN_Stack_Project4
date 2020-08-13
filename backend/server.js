8ac7ccc1be299db77db3fb0327e9165a2479e689
Noelle Everage <neverage84@yahoo.com>
2020-08-13 14:34:01 -0700

    added status.xml and fs to read file

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,16 @@ app.listen(PORT, () => {
  console.log(`Server is listening on port: ${PORT}`);
 });
 
-var parseString = require('xml2js').parseString;
-var xml = "<root>Hello xml2js!</root>"
-parseString(xml, function (err, result) {
-    console.dir(result);
+var fs = require('fs'),
+    xml2js = require('xml2js');
+var xml;
+ 
+var parser = new xml2js.Parser();
+fs.readFile(__dirname + '/status.xml', function(err, data) {
+    parser.parseString(data, function (err, result) {
+        
+        xml = result;
+        console.log(xml);
+        console.log('Done');
+    });
 });
