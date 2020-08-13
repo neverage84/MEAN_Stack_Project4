3f0dd099f5d6db69fe0a0e999c5db8cc6b163e69
Noelle Everage <neverage84@yahoo.com>
2020-08-13 15:02:45 -0700

    Added code to server to request status.xml every 60 seconds

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
-const express = require("express");
+//requiring dependencies
+const express = require("express"),
+                fs = require('fs'),
+                    xml2js = require('xml2js');
 
+//setting up port and running express
 const PORT = 4000;
 const app = express();
 
@@ -7,20 +11,31 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
+//Listening on above report
 app.listen(PORT, () => {
  console.log(`Server is listening on port: ${PORT}`);
 });
 
-var fs = require('fs'),
-    xml2js = require('xml2js');
+//variable to capture received xml file and create new parsing intance
 var xml;
- 
 var parser = new xml2js.Parser();
-fs.readFile(__dirname + '/status.xml', function(err, data) {
-    parser.parseString(data, function (err, result) {
-        
-        xml = result;
-        console.log(xml);
-        console.log('Done');
+
+//Function to request Status.xml
+function StatusXMLRequest(){
+
+//read the status.xml file then perform a function
+    fs.readFile(__dirname + '/status.xml', function(err, data) {
+    //parse the data, store as xml variable, then log that file has been read
+        parser.parseString(data, function (err, result) {
+            xml = result;
+            console.log(xml);
+            console.log('Status.xml Request Sent');
     });
 });
+}
+
+//When server loads, run the StatusXMLfunction
+StatusXMLRequest();
+
+//Run the Request function every 60 seconds
+setInterval(StatusXMLRequest, 60*1000);
